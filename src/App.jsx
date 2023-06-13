ea8cf360d054e7d36cecd450b4a2fe67efa494e6
Paul Holt <paul-ho@lightflows.co.uk>
2023-06-13 23:08:34 +0100

    Test data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import Title from "./components/title.jsx";
-
+import SlideInCard from "./components/slideInCard.jsx";
+import Timeline from "./components/timeline.jsx";
 const App = () => {
+    const events = [
+        {
+            "heading": "University of the West of England",
+            "subheading": "Sept 2018 - June 2021",
+            "text": "First Class Bachelor of Science, Business Computing",
+            "color": "bg-blue-800",
+            "icon": "fa-solid fa-graduation-cap"
+        },
+        {
+            "heading": "Lightflows",
+            "subheading": "Oct 2021 - Present",
+            "text": "Junior Web Developer",
+            "color": "bg-red-700",
+            "icon": "fa-sharp fa-solid fa-briefcase"
+        },
+    ]
+
     return (
         <div className="w-full">
-            <Title></Title>
+            <Title />
+            <div className="w-1/2 mr-0 ml-auto">
+                <SlideInCard right={true}>
+                    <Timeline events={events} />
+                </SlideInCard>
+            </div>
         </div>
     );
 };
