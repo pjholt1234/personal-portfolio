e1867902521d056db935ab5e04998d9b6ef15b28
Paul Holt <paul-ho@lightflows.co.uk>
2023-07-27 00:07:31 +0100

    Update file imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,30 +1,44 @@
 //Dissertation
-import disLoginPage from './dissertation-images/login.jpg';
-import disPlanning1 from './dissertation-images/planning1.jpg';
-import disPlanning2 from './dissertation-images/planning2.jpg';
-import disPlanning3 from './dissertation-images/planning3.jpg';
-import disProfile1 from './dissertation-images/profile1.jpg';
-import disProfile2 from './dissertation-images/profile2.jpg';
-import disProfile3 from './dissertation-images/profile3.jpg';
-import disTricks from './dissertation-images/tricks.jpg';
+import disLoginPage from "./dissertation-images/login.jpg";
+import disPlanning1 from "./dissertation-images/planning1.jpg";
+import disPlanning2 from "./dissertation-images/planning2.jpg";
+import disPlanning3 from "./dissertation-images/planning3.jpg";
+import disProfile1 from "./dissertation-images/profile1.jpg";
+import disProfile2 from "./dissertation-images/profile2.jpg";
+import disProfile3 from "./dissertation-images/profile3.jpg";
+import disTricks from "./dissertation-images/tricks.jpg";
+import disReport from "./files/webcompanionreport.pdf";
 
 //Notion Steam App
-import notionSteamApp from './notion-steam-app.png';
+import notionSteamApp from "./notion-steam-app.png";
 
-//Portfolio
-import portfolio1 from './portfolio-images/portfolio1.png';
-import portfolio2 from './portfolio-images/portfolio2.png';
+//PortfolioV2
+import portfolio1 from "./portfolio-images/portfolio1.png";
+import portfolio2 from "./portfolio-images/portfolio2.png";
+
+//PortfolioV1
+import homepage from "./portfolio-v1/homepage.png";
+import about from "./portfolio-v1/about.png";
+import about2 from "./portfolio-v1/about2.png";
+import contact from "./portfolio-v1/contact.png";
+import projectExample from "./portfolio-v1/projectExample.png";
 
 export {
-    disLoginPage,
-    disPlanning1,
-    disPlanning2,
-    disPlanning3,
-    disProfile1,
-    disProfile2,
-    disProfile3,
-    disTricks,
-    notionSteamApp,
-    portfolio1,
-    portfolio2,
+  disLoginPage,
+  disPlanning1,
+  disPlanning2,
+  disPlanning3,
+  disProfile1,
+  disProfile2,
+  disProfile3,
+  disTricks,
+  notionSteamApp,
+  portfolio1,
+  portfolio2,
+  disReport,
+  homepage,
+  about,
+  about2,
+  contact,
+  projectExample,
 };
