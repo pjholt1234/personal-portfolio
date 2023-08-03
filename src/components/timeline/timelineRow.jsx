39dab24eb110ef969a33c4753027c2a05b3f8d0d
Paul Holt <paul-ho@lightflows.co.uk>
2023-08-03 23:46:35 +0100

    Hard code timeline row count

--- src/components/timeline/timelineRow.jsx
+++ src/components/timeline/timelineRow.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 const TimelineRow = ({ children, className = "", colCount = 8 }) => {
-  const gridCols = `grid-cols-${colCount}`;
+  const gridCols = `grid-cols-8`;
 
   return (
     <div className={`${className} grid ${gridCols} h-[50px]`}>
